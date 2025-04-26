f645785b46769a814fcf54b020fdd8060a26fffe
Ishu Verma <iv891778@gmail.com>
2025-04-27 00:48:30 +0530

    Add files via upload

---
+++ assignment5/task2.py
@@ -0,0 +1,9 @@
+# Task 2: Demonstrate List Slicing 
+
+L1 = list(range(1,11))
+print("Original list:",L1)
+
+Extracted_list= L1[:5]
+print("Extracted first five elements:",Extracted_list)
+
+print("Reverse of the Extracted elements:",Extracted_list[: :-1])
